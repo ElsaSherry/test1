f149c2bf0898a9b8cfce72dcd288519748c0a0c5
asher <asher@DESKTOP-TVKQ1TM>
2024-03-28 12:01:12 +0530

    commited

---
+++ Mavenproject2/src/main/java/maven2/Parent1.java
@@ -0,0 +1,15 @@
+package maven2;
+
+public class Parent1
+{
+public void display()
+{
+	System.out.println("Hello world");
+	
+}
+public static void main(String args[])
+{
+	Parent1 obj=new Parent1();
+	obj.display();
+}
+}
